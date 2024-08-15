7931cb9f0d857760e2ab5aed46315ce0d1376f30
Ukaegbu Gray Nneji <graynneji@outlook.com>
2024-08-15 02:35:46 -0700

    navmenu

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -2,11 +2,13 @@
 import { Provider } from "react-redux";
 import Nav from "./Nav";
 import store from "../store/store";
+import NavMenu from "./NavMenu";
 
 function Header() {
   return (
     <Provider store={store}>
       <Nav />
+      <NavMenu />
     </Provider>
   );
 }
