e92a8456f7f24555382d27bb5cecfab918b4296f
Ukaegbu Gray Nneji <graynneji@outlook.com>
2024-10-08 13:53:43 -0700

    verify

--- app/(landingPage)/verify/page.js
+++ app/(landingPage)/verify/page.js
@@ -1,19 +1,19 @@
-"use client";
-import VerifyEmail from "@/app/_components/VerifyEmail";
-import { useSearchParams } from "next/navigation";
-import { Suspense, lazy } from "react";
+// "use client";
+// import VerifyEmail from "@/app/_components/VerifyEmail";
+// import { useSearchParams } from "next/navigation";
+// import { Suspense, lazy } from "react";
 
-const VerifyEmailWrapper = lazy(() => {
-  const searchParams = useSearchParams();
-  const emailId = searchParams?.get("emailId");
+// const VerifyEmailWrapper = lazy(() => {
+//   const searchParams = useSearchParams();
+//   const emailId = searchParams?.get("emailId");
 
-  return <VerifyEmail emailId={emailId} />;
-});
+//   return <VerifyEmail emailId={emailId} />;
+// });
 
-export default function Page() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <VerifyEmailWrapper />
-    </Suspense>
-  );
-}
+// export default function Page() {
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <VerifyEmailWrapper />
+//     </Suspense>
+//   );
+// }
